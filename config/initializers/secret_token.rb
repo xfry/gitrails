d5c32e22d6818231b4b9870d24d6400fb8f050dd
root <root@xfry-rex.(none)>
2012-08-29 10:43:26 -0500

    Primera Version prueba

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitrails::Application.config.secret_token = '3c368f50b384f878b4f0dc8667489c43c2bcb50cc2706c5db9ab38e42586036409a6cdca960c8e989bb3ef0036158c12c43f81295f5cf53683c57f66df5d17b1'
